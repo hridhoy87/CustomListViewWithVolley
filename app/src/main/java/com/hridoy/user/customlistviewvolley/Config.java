2cefacf09d382f395db534b6a7308b15071338f4
Hridoy <m.asaduzzaman.29@gmail.co>
2017-04-15 02:38:16 +0600

    enter the commit message for your changes. Lines starting Dependencies Changed

--- app/src/main/java/com/hridoy/user/customlistviewvolley/Config.java
+++ app/src/main/java/com/hridoy/user/customlistviewvolley/Config.java
@@ -3,6 +3,20 @@ package com.hridoy.user.customlistviewvolley;
 /**
  * Created by User on 15-Apr-17.
  */
-
+/**
+ * Created by Belal on 11/9/2015.
+ */
 public class Config {
+    //URL of my API
+    public static final String DATA_URL = "http://simplifiedcoding.16mb.com/SuperHeroes/superheroes.php";
+
+    //Tags for my JSON
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_RANK = "rank";
+    public static final String TAG_REAL_NAME = "realName";
+    public static final String TAG_CREATED_BY = "createdBy";
+    public static final String TAG_FIRST_APPEARANCE = "firstAppearance";
+    public static final String TAG_POWERS = "powers";
+
 }
